56e7b78619121b189647660b5f6f3dd1c8938a70
Alexandre Henrique da Silva <echo.alexandre@gmail.com>
2025-05-24 16:21:12 -0300

    initial commit with add spring boot

---
+++ src/main/java/com/example/alexandrehs/picpay_desafio_v1/PicpayDesafioV1Application.java
@@ -0,0 +1,13 @@
+package com.example.alexandrehs.picpay_desafio_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDesafioV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDesafioV1Application.class, args);
+	}
+
+}
